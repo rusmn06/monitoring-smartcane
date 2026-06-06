c363e5245a0a42670eb385098765cdb630fd3e62
rusmn06 <rusmana.deni06@gmail.com>
2026-06-06 10:43:00 +0800

    update commit

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 
 // ── BROKER MQTT (HiveMQ Cloud)
-const MQTT_HOST      = "xxxx.s1.eu.hivemq.cloud"; // URL broker
+const MQTT_HOST      = "f5caf23808cf459197019bc93cf4c619.s1.eu.hivemq.cloud"; // URL broker
 const MQTT_PORT      = 8884;                        // Port WebSocket SSL
-const MQTT_USER      = "USERNAME_HIVEMQ";           // Username HiveMQ
-const MQTT_PASS      = "PASSWORD_HIVEMQ";           // Password HiveMQ
+const MQTT_USER      = "desvi";           // Username HiveMQ
+const MQTT_PASS      = "TAku@2026";           // Password HiveMQ
 const MQTT_CLIENT_ID = "smartcane-web-" + Math.random().toString(16).substr(2, 6);
+
 
 // ── TOPIK MQTT (harus sama dengan kode Arduino/ESP32)
 const TOPIK_STATUS   = "smartstick/status";
@@ -13,8 +14,8 @@ const TOPIK_GPS      = "smartstick/gps";
 const TOPIK_CARI     = "smartstick/cari";
 
 // ── PENGATURAN PETA (Leaflet / OpenStreetMap)
-const PETA_LAT_AWAL  = -3.3194;    // Koordinat awal peta (latitude)
-const PETA_LNG_AWAL  = 114.5908;   // Koordinat awal peta (longitude)
+const PETA_LAT_AWAL  = -3.7738132;    // Koordinat awal peta (latitude)
+const PETA_LNG_AWAL  = 114.7816116;   // Koordinat awal peta (longitude)
 const PETA_ZOOM_AWAL = 13;         // Level zoom saat pertama buka
 const PETA_ZOOM_GPS  = 16;         // Level zoom saat GPS diterima
 
